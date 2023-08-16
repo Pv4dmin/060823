eaec61abfa8b49875331990a0a1451591415dca4
Rem <Pvadmin@mail.ru>
2023-08-16 17:55:52 +0800

    на вход трёхзначное число и на выходе показывает вторую цифру этого числа

---
+++ Hwork1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+void ThreeDigit(int num)
+{
+    if (num > 99 && num < 1000)
+    {
+        Console.WriteLine($"{num / 10 % 10}");
+    }
+    else
+        Console.WriteLine("Сказано же трёхзначное, а ты что ввёл? Эх грамотей.)");
+}
+
+Console.WriteLine("Введите трёхзначное число");
+int number = int.Parse(Console.ReadLine()!);
+ThreeDigit(number);
