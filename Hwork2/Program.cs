85c2ef8c85b9e09d193a84a0b807452875ab2f88
Rem <Pvadmin@mail.ru>
2023-08-17 17:59:29 +0800

    посиск третьей цивре в введённом числе

---
+++ Hwork2/Program.cs
@@ -0,0 +1,26 @@
+﻿void ThreeDigit(int num)
+{
+    if (num > 99 & num < 1000)
+        Console.WriteLine($"Третьей цифрой в этом числе является цифра: {num % 10}");
+    if (num < 100)
+    {
+        Console.WriteLine($"Третей цифры в числе {num} нет");
+    }
+    else if (num > 1000)
+    {
+        while (num > 1000)
+        {
+            num = num / 10;
+        }
+        Console.WriteLine($"Третьей цифрой в этом числе является цифра: {num % 10}");
+    }
+}
+
+int InputNum(string message)
+{
+Console.Write(message);
+return int.Parse(Console.ReadLine()!);
+}
+
+int number = InputNum("Введи любое число друг, и я покажу тебе волшебство ");
+ThreeDigit(number);
