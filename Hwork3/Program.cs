098a941b89e2281bf0ce9dcf8fe59a4706ed8117
Rem <Pvadmin@mail.ru>
2023-08-17 18:47:31 +0800

    Определение выходной день или нет по введённой цифре

---
+++ Hwork3/Program.cs
@@ -0,0 +1,12 @@
+﻿int InputWeekNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int number = InputWeekNum("Введи номер дня недели и я скажу выходной это или нет ");
+if (number == 6 || number == 7)
+{
+    Console.WriteLine("Да, это выходной");
+}
+else Console.WriteLine("Нет, это не выходной");
